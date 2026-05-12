72121f4ec57d141ed79fe6249186372485f5834f
Jorge Alberto Reynaga Jiménez <jorge.reynaga@sesaj.org>
2026-05-12 15:23:39 -0600

    corregir import en urls.py

--- nom035/urls.py
+++ nom035/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,re_path,include
-from surveys.views import 
+from surveys.views import *
 from surveys.stripe_views import (
     StripePlansView, StripeCheckoutView, StripePortalView,
     StripeWebhookView, PaymentSuccessView, PaymentCancelView
