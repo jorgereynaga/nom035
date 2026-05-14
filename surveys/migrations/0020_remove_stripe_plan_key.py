0d1742d026f64252292a4d40ac039fe1f516d9fb
Jorge Alberto Reynaga Jiménez <jorge.reynaga@sesaj.org>
2026-05-14 16:14:47 -0600

    fix: migration 0020 adds stripe fields, gunicorn preload

--- surveys/migrations/0020_remove_stripe_plan_key.py
+++ surveys/migrations/0020_remove_stripe_plan_key.py
@@ -1,4 +1,4 @@
-from django.db import migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
@@ -8,8 +8,24 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
+        migrations.AddField(
+            model_name='userapp',
+            name='stripe_customer_id',
+            field=models.CharField(blank=True, max_length=100, null=True, verbose_name='Stripe Customer ID'),
+        ),
+        migrations.AddField(
+            model_name='userapp',
+            name='stripe_subscription_id',
+            field=models.CharField(blank=True, max_length=100, null=True, verbose_name='Stripe Subscription ID'),
+        ),
+        migrations.AddField(
             model_name='userapp',
             name='stripe_plan_key',
+            field=models.CharField(blank=True, default='', max_length=100, verbose_name='Plan activo'),
+        ),
+        migrations.AddField(
+            model_name='userapp',
+            name='psico_evaluaciones_disponibles',
+            field=models.IntegerField(default=0, verbose_name='Evaluaciones psicométricas disponibles'),
         ),
     ]
